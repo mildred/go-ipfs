d2d581a0e7c07e9173008ed2c59aa6647904c3bd
Jeromy <jeromyj@gmail.com>
2014-09-22 15:14:19 -0700

    catch ipns branch up to master and make all things compile

--- cmd/ipfs/add.go
+++ cmd/ipfs/add.go
@@ -45,7 +45,6 @@ func addCmd(c *commander.Command, inp []string) error {
 	cmd.Opts["r"] = c.Flag.Lookup("r").Value.Get()
 	err := daemon.SendCommand(cmd, "localhost:12345")
 	if err != nil {
-		u.DOut("got err: %v\n", err)
 		u.DOut("Executing command locally.\n")
 		// Do locally
 		conf, err := getConfigDir(c.Parent)
--- crypto/spipe/handshake.go
+++ crypto/spipe/handshake.go
@@ -218,8 +218,6 @@ func (s *SecurePipe) handleSecureIn(hashType string, tIV, tCKey, tMKey []byte) {
 	for {
 		data, ok := <-s.insecure.In
 		if !ok {
-			u.DOut("Closing incoming proxy.\n")
-			close(secureIn)
 			return
 		}
 
@@ -256,8 +254,6 @@ func (s *SecurePipe) handleSecureOut(hashType string, mIV, mCKey, mMKey []byte) 
 	for {
 		data, ok := <-s.Out
 		if !ok {
-			u.DOut("Closing outgoing proxy.\n")
-			close(secureOut)
 			return
 		}
 
